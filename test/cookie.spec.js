099d6fb80780f3d5b14fcf378c06b0d700819cc1
Harminder Virk <virk.officials@gmail.com>
2017-04-05 22:57:22 +0530

    refactor(test): move to japa over mocha

--- test/cookie.spec.js
+++ test/cookie.spec.js
@@ -6,20 +6,16 @@
  * MIT Licensed
 */
 
-/* global it, describe*/
-const chai = require('chai')
-const supertest = require('co-supertest')
-const expect = chai.expect
+const test = require('japa')
+const supertest = require('supertest')
 const http = require('http')
 const sig = require('cookie-signature')
 const Keygrip = require('keygrip')
 const Cookie = require('../')
 const queryString = require('querystring')
 
-require('co-mocha')
-
-describe('Cookie', function () {
-  it('should return an empty object when no cookies have been set', function * () {
+test.group('Cookie', function () {
+  test('should return an empty object when no cookies have been set', async function (assert) {
     const server = http.createServer(function (req, res) {
       const cookies = Cookie.parse(req)
       res.writeHead(200, {'content-type': 'application/json'})
@@ -27,11 +23,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.body.cookies).deep.equal({})
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.body.cookies, {})
   })
 
-  it('should parse plain cookies for a given request when secret is not set', function * () {
+  test('should parse plain cookies for a given request when secret is not set', async function (assert) {
     const server = http.createServer(function (req, res) {
       const cookies = Cookie.parse(req)
       res.writeHead(200, {'content-type': 'application/json'})
@@ -39,11 +35,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['user=foo']).expect(200).end()
-    expect(res.body.cookies).deep.equal({user: 'foo'})
+    const res = await supertest(server).get('/').set('Cookie', ['user=foo']).expect(200)
+    assert.deepEqual(res.body.cookies, {user: 'foo'})
   })
 
-  it('should parse and unsing cookies when secret key is present', function * () {
+  test('should parse and unsing cookies when secret key is present', async function (assert) {
     const SECRET = 'bubblegum'
     const cookieVal = sig.sign('foo', SECRET)
 
@@ -54,11 +50,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['user=' + cookieVal]).expect(200).end()
-    expect(res.body.cookies).deep.equal({user: 'foo'})
+    const res = await supertest(server).get('/').set('Cookie', ['user=' + cookieVal]).expect(200)
+    assert.deepEqual(res.body.cookies, {user: 'foo'})
   })
 
-  it('should decrypt and unsign cookies when decrypt option is set', function * () {
+  test('should decrypt and unsign cookies when decrypt option is set', async function (assert) {
     const SECRET = 'bubblegum'
     const keygrip = new Keygrip([SECRET])
     const cookieVal = sig.sign('foo', SECRET)
@@ -71,11 +67,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['user=' + encryptedVal]).expect(200).end()
-    expect(res.body.cookies).deep.equal({user: 'foo'})
+    const res = await supertest(server).get('/').set('Cookie', ['user=' + encryptedVal]).expect(200)
+    assert.deepEqual(res.body.cookies, {user: 'foo'})
   })
 
-  it('should not return cookie value when unable to decrypt cookie', function * () {
+  test('should not return cookie value when unable to decrypt cookie', async function (assert) {
     const SECRET = 'bubble'
     const keygrip = new Keygrip([SECRET])
     const cookieVal = sig.sign('foo', SECRET)
@@ -88,11 +84,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['user=' + encryptedVal]).expect(200).end()
-    expect(res.body.cookies).deep.equal({})
+    const res = await supertest(server).get('/').set('Cookie', ['user=' + encryptedVal]).expect(200)
+    assert.deepEqual(res.body.cookies, {})
   })
 
-  it('should decrypt and unsign cookies from an array', function * () {
+  test('should decrypt and unsign cookies from an array', async function (assert) {
     const SECRET = 'bubblegum'
     const keygrip = new Keygrip([SECRET])
     const cookieVal = sig.sign('j:' + JSON.stringify([1, 2, 3]), SECRET)
@@ -105,11 +101,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['cart=' + encryptedVal]).expect(200).end()
-    expect(res.body.cookies).deep.equal({cart: [1, 2, 3]})
+    const res = await supertest(server).get('/').set('Cookie', ['cart=' + encryptedVal]).expect(200)
+    assert.deepEqual(res.body.cookies, {cart: [1, 2, 3]})
   })
 
-  it('should parse plain cookies from an array', function * () {
+  test('should parse plain cookies from an array', async function (assert) {
     const server = http.createServer(function (req, res) {
       const cookies = Cookie.parse(req)
       res.writeHead(200, {'content-type': 'application/json'})
@@ -117,11 +113,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['cart=j:' + JSON.stringify([1, 2, 3])]).expect(200).end()
-    expect(res.body.cookies).deep.equal({cart: [1, 2, 3]})
+    const res = await supertest(server).get('/').set('Cookie', ['cart=j:' + JSON.stringify([1, 2, 3])]).expect(200)
+    assert.deepEqual(res.body.cookies, {cart: [1, 2, 3]})
   })
 
-  it('should return undefined when cookie starts with j: but is not a valid object', function * () {
+  test('should return undefined when cookie starts with j: but is not a valid object', async function (assert) {
     const server = http.createServer(function (req, res) {
       const cookies = Cookie.parse(req)
       res.writeHead(200, {'content-type': 'application/json'})
@@ -129,11 +125,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['cart=j:something']).expect(200).end()
-    expect(res.body.cookies).deep.equal({})
+    const res = await supertest(server).get('/').set('Cookie', ['cart=j:something']).expect(200)
+    assert.deepEqual(res.body.cookies, {})
   })
 
-  it('should parse and unsign cookies from an array', function * () {
+  test('should parse and unsign cookies from an array', async function (assert) {
     const SECRET = 'bubblegum'
     const cookieVal = sig.sign('j:' + JSON.stringify([1, 2, 3]), SECRET)
 
@@ -144,22 +140,22 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').set('Cookie', ['cart=' + cookieVal]).expect(200).end()
-    expect(res.body.cookies).deep.equal({cart: [1, 2, 3]})
+    const res = await supertest(server).get('/').set('Cookie', ['cart=' + cookieVal]).expect(200)
+    assert.deepEqual(res.body.cookies, {cart: [1, 2, 3]})
   })
 
-  it('should create plain cookies to be set on response', function * () {
+  test('should create plain cookies to be set on response', async function (assert) {
     const server = http.createServer(function (req, res) {
       Cookie.create(req, res, 'name', 'foo')
       res.writeHead(200, {'content-type': 'application/json'})
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['name=foo'])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['name=foo'])
   })
 
-  it('should remove old cookie value when trying to set cookie multiple times', function * () {
+  test('should remove old cookie value when trying to set cookie multiple times', async function (assert) {
     const server = http.createServer(function (req, res) {
       Cookie.create(req, res, 'name', 'foo')
       Cookie.create(req, res, 'name', 'bar')
@@ -167,11 +163,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['name=bar'])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['name=bar'])
   })
 
-  it('should create signed cookies to be set on response', function * () {
+  test('should create signed cookies to be set on response', async function (assert) {
     const SECRET = 'bubblegum'
     const valueToBe = sig.sign('foo', SECRET)
 
@@ -181,11 +177,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['name=' + queryString.escape(valueToBe)])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['name=' + queryString.escape(valueToBe)])
   })
 
-  it('should create signed and encrypted cookies to be set on response', function * () {
+  test('should create signed and encrypted cookies to be set on response', async function (assert) {
     const SECRET = 'bubblegum'
     const keygrip = new Keygrip([SECRET])
     let valueToBe = sig.sign('foo', SECRET)
@@ -197,11 +193,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['name=' + queryString.escape(valueToBe)])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['name=' + queryString.escape(valueToBe)])
   })
 
-  it('should set multiple cookies on response', function * () {
+  test('should set multiple cookies on response', async function (assert) {
     const SECRET = 'bubblegum'
     const keygrip = new Keygrip([SECRET])
     let valueToBe = sig.sign('foo', SECRET)
@@ -217,11 +213,11 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['name=' + queryString.escape(valueToBe), 'age=' + queryString.escape(ageToBe)])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['name=' + queryString.escape(valueToBe), 'age=' + queryString.escape(ageToBe)])
   })
 
-  it('should set object as a cookie value', function * () {
+  test('should set object as a cookie value', async function (assert) {
     const SECRET = 'bubblegum'
     const keygrip = new Keygrip([SECRET])
     const data = {name: 'foo', age: 22}
@@ -234,18 +230,18 @@ describe('Cookie', function () {
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['user=' + queryString.escape(valueToBe)])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['user=' + queryString.escape(valueToBe)])
   })
 
-  it('should set cookie when object is empty', function * () {
+  test('should set cookie when object is empty', async function (assert) {
     const server = http.createServer(function (req, res) {
       Cookie.create(req, res, 'age', {})
       res.writeHead(200, {'content-type': 'application/json'})
       res.end()
     })
 
-    const res = yield supertest(server).get('/').expect(200).end()
-    expect(res.headers['set-cookie']).deep.equal(['age=' + queryString.escape('j:' + JSON.stringify({}))])
+    const res = await supertest(server).get('/').expect(200)
+    assert.deepEqual(res.headers['set-cookie'], ['age=' + queryString.escape('j:' + JSON.stringify({}))])
   })
 })
